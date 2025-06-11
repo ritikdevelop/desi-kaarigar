dad9ec8a53567dad063182f14cf7a5a593f86c96
ritikdevelop <hrithikrana216@gmail.com>
2025-06-11 11:46:27 +0530

    feat: implement main App component with routing and header

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
