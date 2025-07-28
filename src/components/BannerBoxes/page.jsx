5a34366e24769b16016eb51b7f28fc4d709bfc33
ritikdevelop <hrithikrana216@gmail.com>
2025-07-28 15:40:33 +0530

    refactor: improve code readability by formatting JSX in BannerBoxes component

--- src/components/BannerBoxes/page.jsx
+++ src/components/BannerBoxes/page.jsx
@@ -5,21 +5,31 @@ import { Link } from "react-router-dom";
 const BannerBoxes = (props) => {
   return (
     <div className="bannerBoxes w-full overflow-hidden rounded-md group relative">
-      <img src={props.image} alt="bannerbox image" className="w-full transition-all duration-300 group-hover:scale-105" />
+      <img
+        src={props.image}
+        alt="bannerbox image"
+        className="w-full transition-all duration-300 group-hover:scale-105"
+      />
 
-      <div className={`info absolute p-5 top-0 ${props.info ==="left" ? 'left-0' : 'right-0'} w-[50%] h-[100%] z-50 flex items-center justify-center flex-col gap-2`}>
+      <div
+        className={`info absolute p-5 top-0 ${
+          props.info === "left" ? "left-0" : "right-0"
+        } w-[50%] h-[100%] z-50 flex items-center justify-center flex-col gap-2`}
+      >
         <h2 className="text-[20px] font-[600] ">Mern Stack Web</h2>
 
-        <span className="text-[25px] text-red-400 font-[600] w-full ">$100.00</span>
+        <span className="text-[25px] text-red-400 font-[600] w-full ">
+          $100.00
+        </span>
 
         <div className="w-full text-[14px] font-[600] hover:transition-all hover:duration-300 hover:transform hover:scale-105 ">
-          <Link className="underline  " to='/'>SHOP NOW</Link>
+          <Link className="underline  " to="/">
+            SHOP NOW
+          </Link>
         </div>
       </div>
-
-
     </div>
   );
-}; 
+};
 
 export default BannerBoxes;
