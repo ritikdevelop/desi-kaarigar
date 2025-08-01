ebd5645639aedbd91657eb239724e858ef02d334
ritikdevelop <hrithikrana216@gmail.com>
2025-08-01 13:07:36 +0530

    Updated the App.jsx File

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductListing from "./pages/ProductListing/page";
 import Footer from "./components/Footer/page";
+import FluidCursor from "./components/CustomCursor/page";
+import ProductDetails from "./pages/ProductDetails/page";
 
 function App() {
   return (
     <>
+    {/* <FluidCursor /> */}
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<ProductListing />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
         </Routes>
         <Footer />
       </BrowserRouter>
