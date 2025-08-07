1843b44a8e8905b4e641e4f1be5ceacd5d34f824
ritikdevelop <hrithikrana216@gmail.com>
2025-08-07 11:57:37 +0530

    Updated product item layout

--- src/components/ProductItem/page.jsx
+++ src/components/ProductItem/page.jsx
@@ -1,37 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Rating from "@mui/material/Rating";
 import Button from "@mui/material/Button";
 import { FaRegHeart } from "react-icons/fa";
 import { IoGitCompareOutline } from "react-icons/io5";
 import { MdZoomOutMap } from "react-icons/md";
-
+import { MyContext } from "../../App";
 
 const ProductItem = () => {
+  const context = useContext(MyContext);
+
   return (
     <div className="productItem shadow-lg rounded-md overflow-hidden border border-gray-200">
       <div className="group imgWrapper w-[100%] h-[200px] overflow-hidden rounded-md relative">
-        <img
-          src="https://www.fnp.com/images/pr/l/v300/delectable-chocolates-in-floral-box-21-pcs_1.jpg"
-          alt="img"
-          className="w-full"
-        />
+        <Link to="/product/84758">
+          <img
+            src="https://www.fnp.com/images/pr/l/v300/delectable-chocolates-in-floral-box-21-pcs_1.jpg"
+            alt="img"
+            className="w-full"
+          />
+        </Link>
         <span className="discount flex items-center absolute top-[10px] left-[10px] z-50 bg-red-400 text-white rounded-md p-1 text-[12px] font-[400]">
           10%
         </span>
 
         <div className="actions absolute top-[-200px] right-[5px] z-50 flex items-center gap-4 flex-col w-[50px] transition-all duration-800 group-hover:top-[15px] opacity-0 group-hover:opacity-100">
+
+          <Button className="!w-[35px] !h-[35px] !min-w-[35px] hover:text-white !rounded-full !bg-white text-black hover:!bg-red-500 group"
+          onClick={() => context.setOpenProductDetailsModal(true)}>
+            <MdZoomOutMap className="text-[18px] text-black group-hover:text-white hover:!text-white" />
+          </Button>
+
           <Button className="!w-[35px] !h-[35px] !min-w-[35px] hover:text-white !rounded-full !bg-white text-black hover:!bg-red-500 group">
             <FaRegHeart className="text-[18px] text-black group-hover:text-white hover:!text-white" />
           </Button>
 
-          <Button className="!w-[35px] !h-[35px] !min-w-[35px] hover:text-white !rounded-full !bg-white text-black hover:!bg-red-500 group">
+          {/* <Button className="!w-[35px] !h-[35px] !min-w-[35px] hover:text-white !rounded-full !bg-white text-black hover:!bg-red-500 group">
             <IoGitCompareOutline className="text-[18px] text-black group-hover:text-white hover:!text-white" />
-          </Button>
+          </Button> */}
 
-          <Button className="!w-[35px] !h-[35px] !min-w-[35px] hover:text-white !rounded-full !bg-white text-black hover:!bg-red-500 group">
-            <MdZoomOutMap className="text-[18px] text-black group-hover:text-white hover:!text-white" />
-          </Button>
+          
         </div>
       </div>
 
