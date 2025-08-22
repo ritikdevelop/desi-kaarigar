1a04faf4b4d8a87a3a32bb7198463ce72b2ccde4
ritikdevelop <hrithikrana216@gmail.com>
2025-08-22 11:57:27 +0530

    enhance Header component with user account menu and login/logout functionality

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import Search from "../Search";
 import Badge from "@mui/material/Badge";
 import { styled } from "@mui/material/styles";
 import IconButton from "@mui/material/IconButton";
 import { MdOutlineShoppingCart } from "react-icons/md";
-import { FaRegHeart } from "react-icons/fa";
-import Tooltip from '@mui/material/Tooltip';
+import { FaRegHeart, FaRegUser } from "react-icons/fa";
+import Tooltip from "@mui/material/Tooltip";
 import Navigation from "./Navigation";
+import { MyContext } from "../../App";
+import Button from "@mui/material/Button";
+
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { IoBagCheckOutline } from "react-icons/io5";
+import { IoIosLogOut, IoMdHeartEmpty } from "react-icons/io";
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {
@@ -19,16 +26,25 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
 }));
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const context = useContext(MyContext);
   return (
     <header className="bg-white">
-
       {/* //Todo: Top Strip */}
       <div className="top-strip py-2 border-t-[1px] border-gray-300 border-b-[1px]">
         <div className="container">
           <div className="flex items-center justify-between">
             <div className="col1 w-[50%]">
               <p className="text-[14px] font-[400]">
-                Elevate your chocolate experience to new heights with our premium chocolate.
+                Elevate your chocolate experience to new heights with our
+                premium chocolate.
               </p>
             </div>
 
@@ -61,50 +77,151 @@ const Header = () => {
         <div className="container flex items-center justify-between">
           <div className="col1 w-[25%]">
             <Link to={"/"}>
-              <img src="/logo.jpg" alt="logo" />
+              <img src="/newlogo.png" alt="logo" />
             </Link>
           </div>
           <div className="col2 w-[45%]">
             <Search />
           </div>
+
           <div className="col3 w-[30%] flex items-center pl-7">
             <ul className="flex items-center justify-end w-full gap-4">
-              <li className="list-none">
-                <Link
-                  to="/login"
-                  className="link transition text-[15px] font-[400]"
-                >
-                  Login
-                </Link>{" "}
-                /&nbsp;
-                <Link
-                  to="/register"
-                  className="link transition text-[15px] font-[400]"
-                >
-                  Register
-                </Link>
-              </li>
+              {context.isLogin === false ? (
+                <li className="list-none">
+                  <Link
+                    to="/login"
+                    className="link transition text-[15px] font-[400]"
+                  >
+                    Login
+                  </Link>
+                  /&nbsp;
+                  <Link
+                    to="/register"
+                    className="link transition text-[15px] font-[400]"
+                  >
+                    Register
+                  </Link>
+                </li>
+              ) : (
+                <>
+                  <Button
+                    className="!text-black myAccountWrap flex items-center gap-3 cursor-pointer"
+                    onClick={handleClick}
+                  >
+                    <Button className="!w-[20px] !h-[60px] !Min-w-[20px] !rounded-full !bg-[#f1f1f1]">
+                      <FaRegUser className="text-[18px] text-black" />
+                    </Button>
 
+                    <div className="info flex flex-col">
+                      <h4 className="text-[16px] leading-3 text-black mb-0 capitalize text-left justify-start">
+                        Test
+                      </h4>
+                      <span className="text-[13px] text-black capitalize text-left justify-start">
+                        test23@gmail.com
+                      </span>
+                    </div>
+                  </Button>
+
+                  <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    slotProps={{
+                      paper: {
+                        elevation: 0,
+                        sx: {
+                          overflow: "visible",
+                          filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                          mt: 1.5,
+                          "& .MuiAvatar-root": {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                          },
+                          "&::before": {
+                            content: '""',
+                            display: "block",
+                            position: "absolute",
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: "background.paper",
+                            transform: "translateY(-50%) rotate(45deg)",
+                            zIndex: 0,
+                          },
+                        },
+                      },
+                    }}
+                    transformOrigin={{ horizontal: "right", vertical: "top" }}
+                    anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                  >
+                    <Link to="/my-account" className="w-full block">
+                      <MenuItem
+                        onClick={handleClose}
+                        className="flex gap-2 !py-2"
+                      >
+                        <FaRegUser className="text-[18px]" />
+                        <span className="text-[14px]">My Account</span>
+                      </MenuItem>
+                    </Link>
+                    <Link to="/my-orders" className="w-full block">
+                      <MenuItem
+                        onClick={handleClose}
+                        className="flex gap-2 !py-2"
+                      >
+                        <IoBagCheckOutline className="text-[18px]" />
+                        <span className="text-[14px]">My Orders</span>
+                      </MenuItem>
+                    </Link>
+
+                    <Link to="/wishlist" className="w-full block">
+                      <MenuItem
+                        onClick={handleClose}
+                        className="flex gap-2 !py-2"
+                      >
+                        <IoMdHeartEmpty className="text-[18px]" />
+                        <span className="text-[14px]">Wishlist</span>
+                      </MenuItem>
+                    </Link>
+                    <Link to="/" className="w-full block">
+                      <MenuItem
+                        onClick={handleClose}
+                        className="flex gap-2 !py-2"
+                      >
+                        <IoIosLogOut className="text-[18px]" />
+                        <span className="text-[14px]">Logout</span>
+                      </MenuItem>
+                    </Link>
+                  </Menu>
+                </>
+              )}
 
               {/* //todo: Wishlist Icon */}
               <li>
                 <Tooltip title="Wishlist">
-                <IconButton aria-label="cart">
-                  <StyledBadge badgeContent={4} color="secondary">
-                    <FaRegHeart />
-                  </StyledBadge>
-                </IconButton>
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <FaRegHeart />
+                    </StyledBadge>
+                  </IconButton>
                 </Tooltip>
               </li>
 
               {/* //todo: Cart Icon */}
               <li>
                 <Tooltip title="Cart">
-                <IconButton aria-label="cart">
-                  <StyledBadge badgeContent={4} color="secondary">
-                    <MdOutlineShoppingCart />
-                  </StyledBadge>
-                </IconButton>
+                  <IconButton
+                    aria-label="cart"
+                    onClick={() => context.setOpenCartPanel(true)}
+                  >
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <MdOutlineShoppingCart />
+                    </StyledBadge>
+                  </IconButton>
                 </Tooltip>
               </li>
             </ul>
